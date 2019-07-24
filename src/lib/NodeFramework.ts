57e69fa72a991a5a7cf0af9f7e3b7056145f9802
Philam18 <philiplam818@gmail.com>
2019-07-23 21:20:02 -0400

    work on recursive routes resolution  structure

--- src/lib/NodeFramework.ts
+++ src/lib/NodeFramework.ts
@@ -8,18 +8,19 @@ export class NodeFramework
     public static serve()
     {
         let app = express();
+        let routes : Object[] = NodeFramework.parseRoutes(Routes.getRoutes());
 
-        for (let route_config of Routes.getRoutes())
+        for (let route_config of routes)
         {
             let template   : String = route_config.template;
             let method     : String = route_config.method;
             let controller : String = route_config.controller;
             let path       : String = route_config.path;
-
-
-
-
         }
+
+
+
+
         
 
         let port = Config.getPort();
@@ -32,11 +33,20 @@ export class NodeFramework
 
     private static parseRoutes(routes : Object[], properties : Object = {}) : Object[]
     {
-        let result = [];
+        let result : Object[] = [];
 
         for (let route of routes)
         {
-            
+            let new_obj = Object.assign(route,properties);
+            if (new_obj.hasOwnProperty('children') === true)
+            {
+                let children = NodeFramework.parseRoutes(new_obj.children, new_obj);
+                
+            }
+            else
+            {
+                result.push(new_obj);
+            }
         }
         
 
